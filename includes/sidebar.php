0c9866b1ce507dc4d3276fcb6057b8d46b516499
SIVA PRASAD <v.sivaprasad7482@gmail.com>
2021-06-10 12:54:45 +0530

    commit on 'master'

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,13 +4,13 @@
           <div class="card mb-4">
             <h5 class="card-header">Search</h5>
             <div class="card-body">
-                   <form name="search" action="search.php" method="post">
-              <div class="input-group">
+              <form name="search" action="search.php" method="post">
+                <div class="input-group">
            
-        <input type="text" name="searchtitle" class="form-control" placeholder="Search for..." required>
-                <span class="input-group-btn">
-                  <button class="btn btn-secondary" type="submit">Go!</button>
-                </span>
+                  <input type="text" name="searchtitle" class="form-control" placeholder="Search for..." required>
+                    <span class="input-group-btn">
+                      <button class="btn btn-secondary" type="submit">Go!</button>
+                    </span>
               </form>
               </div>
             </div>
@@ -23,10 +23,10 @@
               <div class="row">
                 <div class="col-lg-6">
                   <ul class="list-unstyled mb-0">
-<?php $query=mysqli_query($con,"select id,CategoryName from tblcategory");
-while($row=mysqli_fetch_array($query))
-{
-?>
+                    <?php $query=mysqli_query($con,"select id,CategoryName from tblcategory");
+                    while($row=mysqli_fetch_array($query))
+                    {
+                    ?>
 
                     <li>
                       <a href="category.php?catid=<?php echo htmlentities($row['id'])?>"><?php echo htmlentities($row['CategoryName']);?></a>
