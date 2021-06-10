40f92f019b43840d7a6b26c1dce5955e890549ea
SIVA PRASAD <v.sivaprasad7482@gmail.com>
2021-06-10 12:25:55 +0530

    git push -u origin master

--- admin/add-post.php
+++ admin/add-post.php
@@ -57,6 +57,7 @@ $error="Something went wrong . Please try again.";
 
         <!-- App favicon -->
         <link rel="shortcut icon" href="assets/images/favicon.ico">
+        
         <!-- App title -->
         <title>The Updater | Add Post</title>
 
